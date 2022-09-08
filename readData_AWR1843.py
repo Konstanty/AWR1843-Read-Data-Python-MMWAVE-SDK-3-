5b0defcb826bacd677d01671a4b4636c368e07af
Konstanty Bialkowski <konstanty@ieee.org>
2022-09-08 14:24:54 +1000

    Cleanup whitespace

--- readData_AWR1843.py
+++ readData_AWR1843.py
@@ -12,8 +12,8 @@ configFileName = 'AWR1843config.cfg'
 
 CLIport = {}
 Dataport = {}
-byteBuffer = np.zeros(2**15,dtype = 'uint8')
-byteBufferLength = 0;
+byteBuffer = np.zeros(2**15, dtype='uint8')
+byteBufferLength = 0
 
 
 class MainWindow(QtWidgets.QMainWindow):
@@ -70,14 +70,15 @@ class MainWindow(QtWidgets.QMainWindow):
 # Function to configure the serial ports and send the data from
 # the configuration file to the radar
 def serialConfig(configFileName):
-    
+
     global CLIport
     global Dataport
     # Open the serial ports for the configuration and the data ports
-    
-    # Raspberry pi
-    #CLIport = serial.Serial('/dev/ttyACM0', 115200)
-    #Dataport = serial.Serial('/dev/ttyACM1', 921600)
+
+    # Raspberry Pi/Linux
+    # CLIport = serial.Serial('/dev/ttyACM0', 115200)
+    # Dataport = serial.Serial('/dev/ttyACM1', 921600)
+
     
     # Windows
     CLIport = serial.Serial('COM8', 115200)
@@ -89,26 +90,26 @@ def serialConfig(configFileName):
         CLIport.write((i+'\n').encode())
         print(i)
         time.sleep(0.01)
-        
+
     return CLIport, Dataport
 
-# ------------------------------------------------------------------
 
+# ------------------------------------------------------------------
 # Function to parse the data inside the configuration file
 def parseConfigFile(configFileName):
-    configParameters = {} # Initialize an empty dictionary to store the configuration parameters
-    
+    configParameters = {}  # Initialize an empty dictionary to store the configuration parameters
+
     # Read the configuration file and send it to the board
     config = [line.rstrip('\r\n') for line in open(configFileName)]
     for i in config:
-        
+
         # Split the line
         splitWords = i.split(" ")
-        
+
         # Hard code the number of antennas, change if other configuration is used
         numRxAnt = 4
         numTxAnt = 3
-        
+
         # Get the information about the profile configuration
         if "profileCfg" in splitWords[0]:
             startFreq = int(float(splitWords[2]))
@@ -116,24 +117,23 @@ def parseConfigFile(configFileName):
             rampEndTime = float(splitWords[5])
             freqSlopeConst = float(splitWords[8])
             numAdcSamples = int(splitWords[10])
-            numAdcSamplesRoundTo2 = 1;
-            
-            while numAdcSamples > numAdcSamplesRoundTo2:
-                numAdcSamplesRoundTo2 = numAdcSamplesRoundTo2 * 2;
-                
-            digOutSampleRate = int(splitWords[11]);
-            
-        # Get the information about the frame configuration    
-        elif "frameCfg" in splitWords[0]:
-            
-            chirpStartIdx = int(splitWords[1]);
-            chirpEndIdx = int(splitWords[2]);
-            numLoops = int(splitWords[3]);
-            numFrames = int(splitWords[4]);
-            framePeriodicity = float(splitWords[5]);
+            numAdcSamplesRoundTo2 = 1
 
-            
-    # Combine the read data to obtain the configuration parameters           
+            while numAdcSamples > numAdcSamplesRoundTo2:
+                numAdcSamplesRoundTo2 = numAdcSamplesRoundTo2 * 2
+
+            digOutSampleRate = int(splitWords[11])
+
+        # Get the information about the frame configuration
+        elif "frameCfg" in splitWords[0]:
+
+            chirpStartIdx = int(splitWords[1])
+            chirpEndIdx = int(splitWords[2])
+            numLoops = int(splitWords[3])
+            numFrames = int(splitWords[4])
+            framePeriodicity = float(splitWords[5])
+
+    # Combine the read data to obtain the configuration parameters
     numChirpsPerFrame = (chirpEndIdx - chirpStartIdx + 1) * numLoops
     configParameters["numDopplerBins"] = numChirpsPerFrame / numTxAnt
     configParameters["numRangeBins"] = numAdcSamplesRoundTo2
@@ -142,43 +142,43 @@ def parseConfigFile(configFileName):
     configParameters["dopplerResolutionMps"] = 3e8 / (2 * startFreq * 1e9 * (idleTime + rampEndTime) * 1e-6 * configParameters["numDopplerBins"] * numTxAnt)
     configParameters["maxRange"] = (300 * 0.9 * digOutSampleRate)/(2 * freqSlopeConst * 1e3)
     configParameters["maxVelocity"] = 3e8 / (4 * startFreq * 1e9 * (idleTime + rampEndTime) * 1e-6 * numTxAnt)
-    
-    return configParameters
-   
-# ------------------------------------------------------------------
 
+    return configParameters
+
+
+# ------------------------------------------------------------------
 # Funtion to read and parse the incoming data
 def readAndParseData18xx(Dataport, configParameters):
     global byteBuffer, byteBufferLength
-    
+
     # Constants
-    OBJ_STRUCT_SIZE_BYTES = 12;
-    BYTE_VEC_ACC_MAX_SIZE = 2**15;
-    MMWDEMO_UART_MSG_DETECTED_POINTS = 1;
-    MMWDEMO_UART_MSG_RANGE_PROFILE   = 2;
-    maxBufferSize = 2**15;
-    tlvHeaderLengthInBytes = 8;
-    pointLengthInBytes = 16;
+    OBJ_STRUCT_SIZE_BYTES = 12
+    BYTE_VEC_ACC_MAX_SIZE = 2**15
+    MMWDEMO_UART_MSG_DETECTED_POINTS = 1
+    MMWDEMO_UART_MSG_RANGE_PROFILE   = 2
+    maxBufferSize = 2**15
+    tlvHeaderLengthInBytes = 8
+    pointLengthInBytes = 16
     magicWord = [2, 1, 4, 3, 6, 5, 8, 7]
-    
+
     # Initialize variables
-    magicOK = 0 # Checks if magic number has been read
-    dataOK = 0 # Checks if the data has been read correctly
+    magicOK = 0  # Checks if magic number has been read
+    dataOK = 0  # Checks if the data has been read correctly
     frameNumber = 0
     detObj = {}
-    
+
     readBuffer = Dataport.read(Dataport.in_waiting)
     byteVec = np.frombuffer(readBuffer, dtype = 'uint8')
     byteCount = len(byteVec)
-    
+
     # Check that the buffer is not full, and then add the data to the buffer
     if (byteBufferLength + byteCount) < maxBufferSize:
         byteBuffer[byteBufferLength:byteBufferLength + byteCount] = byteVec[:byteCount]
         byteBufferLength = byteBufferLength + byteCount
-        
+
     # Check that the buffer has some data
     if byteBufferLength > 16:
-        
+
         # Check for all possible locations of the magic word
         possibleLocs = np.where(byteBuffer == magicWord[0])[0]
 
@@ -188,38 +188,38 @@ def readAndParseData18xx(Dataport, configParameters):
             check = byteBuffer[loc:loc+8]
             if np.all(check == magicWord):
                 startIdx.append(loc)
-               
+
         # Check that startIdx is not empty
         if startIdx:
-            
+
             # Remove the data before the first start index
             if startIdx[0] > 0 and startIdx[0] < byteBufferLength:
                 byteBuffer[:byteBufferLength-startIdx[0]] = byteBuffer[startIdx[0]:byteBufferLength]
                 byteBuffer[byteBufferLength-startIdx[0]:] = np.zeros(len(byteBuffer[byteBufferLength-startIdx[0]:]),dtype = 'uint8')
                 byteBufferLength = byteBufferLength - startIdx[0]
-                
+
             # Check that there have no errors with the byte buffer length
             if byteBufferLength < 0:
                 byteBufferLength = 0
-                
+
             # word array to convert 4 bytes to a 32 bit number
             word = [1, 2**8, 2**16, 2**24]
-            
+
             # Read the total packet length
             totalPacketLen = np.matmul(byteBuffer[12:12+4],word)
-            
+
             # Check that all the packet has been read
             if (byteBufferLength >= totalPacketLen) and (byteBufferLength != 0):
                 magicOK = 1
-    
+
     # If magicOK is equal to 1 then process the message
     if magicOK:
         # word array to convert 4 bytes to a 32 bit number
         word = [1, 2**8, 2**16, 2**24]
-        
+
         # Initialize the pointer index
         idX = 0
-        
+
         # Read the header
         magicNumber = byteBuffer[idX:idX+8]
         idX += 8
@@ -242,7 +242,7 @@ def readAndParseData18xx(Dataport, configParameters):
 
         # Read the TLV messages
         for tlvIdx in range(numTLVs):
-            
+
             # word array to convert 4 bytes to a 32 bit number
             word = [1, 2**8, 2**16, 2**24]
 
@@ -260,9 +260,9 @@ def readAndParseData18xx(Dataport, configParameters):
                 y = np.zeros(numDetectedObj,dtype=np.float32)
                 z = np.zeros(numDetectedObj,dtype=np.float32)
                 velocity = np.zeros(numDetectedObj,dtype=np.float32)
-                
+
                 for objectNum in range(numDetectedObj):
-                    
+
                     # Read the data for each object
                     x[objectNum] = byteBuffer[idX:idX + 4].view(dtype=np.float32)
                     idX += 4
@@ -272,29 +272,27 @@ def readAndParseData18xx(Dataport, configParameters):
                     idX += 4
                     velocity[objectNum] = byteBuffer[idX:idX + 4].view(dtype=np.float32)
                     idX += 4
-                
+
                 # Store the data in the detObj dictionary
                 detObj = {"numObj": numDetectedObj, "x": x, "y": y, "z": z, "velocity":velocity}
                 dataOK = 1
-                
- 
+
         # Remove already processed data
-        if idX > 0 and byteBufferLength>idX:
+        if idX > 0 and byteBufferLength > idX:
             shiftSize = totalPacketLen
-            
-                
+
             byteBuffer[:byteBufferLength - shiftSize] = byteBuffer[shiftSize:byteBufferLength]
             byteBuffer[byteBufferLength - shiftSize:] = np.zeros(len(byteBuffer[byteBufferLength - shiftSize:]),dtype = 'uint8')
             byteBufferLength = byteBufferLength - shiftSize
-            
+
             # Check that there are no errors with the buffer length
             if byteBufferLength < 0:
-                byteBufferLength = 0         
+                byteBufferLength = 0
 
     return dataOK, frameNumber, detObj
 
 
-# -------------------------    MAIN   -----------------------------------------  
+# -------------------------    MAIN   -----------------------------------------
 # Configurate the serial port
 CLIport, Dataport = serialConfig(configFileName)
 
